a554108f689636ba59fb7d3f3f68e2fe6c8f5c06
lynnerw <larwitter@gmail.com>
2016-05-26 18:43:41 -0400

    added assets for cool scenarion

--- js/app.js
+++ js/app.js
@@ -15,26 +15,32 @@ $(document).ready(function() {
        {"left": "1020px"},
        500,
        function() {
+         $(this).stop();
          $(this).hide();
          $(this).css("left", "520px");
        }
      );
-  })
+    })
    .mouseup(function() {
      $(".ryu-throwing").hide();
      $(".ryu-ready").show();
-   })
-   .keydown(function() {
-     $(".ryu-ready").hide();
-     $(".ryu-throwing").hide();
-     $(".ryu-still").hide();
-     $(".ryu-cool").show();
-   })
-   .keyup(function() {
-     $(".ryu-cool").hide();
-     $(".ryu-still").show();
-   })
-});
+   });
+
+$(document).keydown(function(e) {
+    if (e.keyCode == 88) {
+      playCool();
+      $(".ryu-throwing").hide();
+      $(".ryu-cool").show();
+    }
+  }).keyup(function(e) {
+    if (e.keyCode == 88) {
+      $("#cool")[0].pause();
+      $("#cool")[0].load();
+      $(".ryu-cool").hide();
+      $(".ryu-still").show();
+     }
+ });
+ })
 function playHadouken() {
   $("#hadouken-sound")[0].volume = 0.5;
   $("#hadouken-sound")[0].load();
